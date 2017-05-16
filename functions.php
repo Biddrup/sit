e9869afc303bc53e04ab3bb81acd7fb767042b96
Biddrup Kumar Mallick <biddrup3@gmail.com>
2017-05-16 13:34:33 +0600

    CSS and JS
    Adding customize css and js

--- functions.php
+++ functions.php
@@ -5,6 +5,9 @@
 	add_action('after_setup_theme','sit_setup');
 
 	function sit_scripts(){
-		wp_enqueue_style('main-css', get_stylesheet_uri(), null, 'v1.0', 'all');
+		wp_enqueue_style('main-css', get_stylesheet_uri(), array('extra-css'), 'v1.0', 'all');
+		wp_enqueue_style('extra-css', get_template_directory_uri().'/css/extra.css', null, 'v1.01', 'all');
+		wp_enqueue_script('jquery');
+		wp_enqueue_script('custom-js', get_template_directory_uri().'/js/custom.js', array('jquery'), 'v2.01', true);
 	}
 	add_action('wp_enqueue_scripts','sit_scripts');
