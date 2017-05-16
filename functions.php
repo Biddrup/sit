82ff31e597d8faef9a221d15086a4c7b6d7b45e0
Biddrup Kumar Mallick <biddrup3@gmail.com>
2017-05-16 13:22:14 +0600

    And going on

--- functions.php
+++ functions.php
@@ -1,2 +1,10 @@
 <?php
-	function 
+	function sit_setup(){
+		add_theme_support('title-tag');
+	}
+	add_action('after_setup_theme','sit_setup');
+
+	function sit_scripts(){
+		wp_enqueue_style('main-css', get_stylesheet_uri(), null, 'v1.0', 'all');
+	}
+	add_action('wp_enqueue_scripts','sit_scripts');
--- header.php
+++ header.php
@@ -2,7 +2,6 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-	<title>Home</title>
 	<?php wp_head(); ?>
 </head>
 <body>
