aaaea51663609f4fa5883d0f74573af6f11e8c38
Biddrup Kumar Mallick <biddrup3@gmail.com>
2017-05-20 05:08:48 +0600

    Custom Post Display
    Working on cutom post display

--- front-page.php
+++ front-page.php
@@ -11,5 +11,24 @@
 			echo dynamic_sidebar('widget_one');
 		}
 	?>
+
+	<section class="custom-post">
+		<div class="all-post">
+			<?php
+				$sit_post = new WP_Query('post_type=sit_product');
+				if ( have_posts()) {
+					while ( have_posts()) {
+						the_post(); ?>
+						<div class="single-post">
+							<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+							<?php the_content(); ?>
+						</div>
+				<?php	}
+				}else{
+					echo "Dont have post";
+				}
+			?>
+		</div>
+	</section>
 	<hr>
 <?php get_footer(); ?>
