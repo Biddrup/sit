448166721fc927a2950c0e7c6e985e54a9655e71
Biddrup Kumar Mallick <biddrup3@gmail.com>
2017-05-20 05:40:40 +0600

    Custom Post Display
    Working on custom post display

--- front-page.php
+++ front-page.php
@@ -15,12 +15,18 @@
 	<section class="custom-post">
 		<div class="all-post">
 			<?php
-				$sit_post = new WP_Query('post_type=sit_product');
-				if ( have_posts()) {
-					while ( have_posts()) {
-						the_post(); ?>
+				$sit_post = new WP_Query(array(
+					'post_type'=>'sit_product',
+					'posts_per_page'=>3,
+				));
+				if ( $sit_post->have_posts()) {
+					while ( $sit_post->have_posts()) {
+						$sit_post->the_post();
+						$email = get_post_meta( get_the_ID(), 'Email', true);
+						?>
 						<div class="single-post">
-							<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+							<h2><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h2>
+							<h3><?php echo $email; ?></h3>
 							<?php the_content(); ?>
 						</div>
 				<?php	}
